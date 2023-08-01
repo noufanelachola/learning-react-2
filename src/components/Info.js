72bcbacb1e2f5ddd08f0658a06a712c0955b5fe0
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-01 16:08:40 +0530

    upto buttons

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Photo from "../assets/photo.jpg";
+import Mail from "../assets/Mail.png";
+import LinkedIn from "../assets/linkedin.png";
 import "./Info.css";
 
 const Info = () => {
@@ -7,6 +9,24 @@ const Info = () => {
         <div className="infoContent">
             <div className="infoPhoto">
                 <img src={Photo} />
+            </div>
+            <div className="mainContent">
+                <p className="info--Name">Noufan Elachola</p>
+                <p className="info--Title">Frontend Developer</p>
+                <p className="info--Web">
+                    <a href="#">noufanelachola.github.io</a>
+                </p>
+
+                <div className="infoBtn">
+                    <button className="btn btnEmail" >
+                        <img src={Mail} />
+                        <p>Email</p>
+                    </button>
+                    <button className="btn btnLinkedIn" onclick="window.location.href='https://www.linkedin.com/in/noufan-elachola-32228a1b0/';"> 
+                        <img src={LinkedIn} />
+                        <p>LinkedIn</p> 
+                    </button>
+                </div>
             </div>
         </div>
     );
