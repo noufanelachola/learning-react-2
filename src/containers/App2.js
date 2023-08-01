966a363e6f988b3bf0c90381454adb20dab1064a
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-01 18:26:59 +0530

    100%

--- src/containers/App2.js
+++ src/containers/App2.js
@@ -1,5 +1,13 @@
 import React from "react";
+import App from "./App";
+import "./App2.css";
 
 const App2 = () => {
-    
+    return(
+        <div className="app2Content">
+            <App/>
+        </div>
+    );
 }
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './containers/App';
 import "./index.css";
+import App2 from './containers/App2';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <App2/>
 );
