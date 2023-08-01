123a6bd8ecd4b91b8506f77f3c17a16eb8b91823
noufanelachola <noufansadiqelachola@gmail.com>
2023-08-01 11:24:39 +0530

    photo added

---
+++ src/components/Info.js
@@ -0,0 +1,11 @@
+import Photo from "../assets/photo.jpg";
+
+const Info = () => {
+    return(
+        <div>
+            <img className="info--photo" src={Photo}/>
+        </div>
+    );
+}
+
+export default Info;
